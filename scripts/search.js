1f2ac124f0d1ef0c25166d50a68dcc093d1d9cca
GuidoCerioni <60760576+GuidoCerioni@users.noreply.github.com>
2020-04-27 19:40:00 -0300

    video

--- scripts/search.js
+++ scripts/search.js
@@ -5,6 +5,10 @@ document.getElementsByClassName("arrowimg")[0]
 
 
 var video = document.getElementById('video');
+console.log(navigator.mediaDevices);
+console.log(navigator.mediaDevices.getUserMedia({ video: true }));
+
+
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
